9ae54b95a39dadfc14edbcfc6d091a0628a6f673
Pedro Allas dos Santos Borges <pedro.borges@faculdadegran.edu.br>
2025-03-27 20:35:02 -0300

    feat(frontend): implementação do frontend

---
+++ frontend/src/components/curriculo/MiniCV.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+
+export default function MiniCV() {
+	return (
+		<div className="flex-1 flex flex-col-reverse gap-6 md:flex-row lg:flex-col-reverse xl:flex-row items-center md:items-start lg:items-center xl:items-start bg-black border border-zinc-800 rounded-2xl px-6 pt-6">
+			<div className="relative min-w-72 h-64 xl:self-end">
+				<Image src="/minha-foto.png" alt="Foto de perfil" fill />
+			</div>
+			<div className="flex flex-col gap-5 self-center py-6 items-center  md:items-start lg:items-center xl:items-start">
+				<div className="flex flex-col items-center md:items-start lg:items-center xl:items-start">
+					<span
+						className="
+                            bg-gradient-to-r from-red-500 via-white to-white
+                            text-transparent bg-clip-text text-2xl font-bold
+                        "
+					>
+						Pedro Allas
+					</span>
+					<span>CTO da PADevs</span>
+				</div>
+				<p className="text-sm text-center md:text-left lg:text-center xl:text-left">
+					Engenheiro de Software, Analista e Desenvolvedor de Sistemas, Professor de
+					Matemática. Fundador da PADevs, com vasta experiência em desenvolvimento de
+					software e educação. Dedica-se a formar novos talentos e a liderar projetos
+					inovadores na área de tecnologia.
+				</p>
+			</div>
+		</div>
+	)
+}
